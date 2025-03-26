37ab8e281d2672c95cacdb8904fd6d97cbe2e11b
Ari Badovska <160342977+AriSnowie97@users.noreply.github.com>
2025-03-26 08:28:16 +0200

    Add files via upload

---
+++ PythonApplication6.py
@@ -0,0 +1,6 @@
+def celsius_to_fahrenheit(celsius):
+    return celsius * 9 / 5 + 32
+    return fahrenheit
+celsius = float(input("Enter temperature in celsius: "))
+fahrenheit = celsius_to_fahrenheit(celsius)
+print(f"{celsius} C is {fahrenheit} F")
